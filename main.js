67646e397c7924e628bafe549bef09020e775aab
oz-elhassid <oz.elhassid@gmail.com>
2021-11-22 17:56:07 +0200

    return string

--- main.js
+++ main.js
@@ -6,4 +6,6 @@ const new_str = core.getInput("new");
 
 const additions = diff.diffTrimmedLines(old_str, new_str).filter((change) => change.added).map((change) => change.value.trim());
 
-core.setOutput("additions", additions);
+const additions_str = additions.toString().replace(',', '\n');
+
+core.setOutput("additions", additions_str);
