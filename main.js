15a6f4b6764aede846485f89227fdf6d050bb39e
Oz Elhassid <45009449+oz-elhassid@users.noreply.github.com>
2021-11-23 11:08:25 +0200

    added more outputs

--- main.js
+++ main.js
@@ -5,7 +5,19 @@ const old_str = core.getInput("old");
 const new_str = core.getInput("new");
 
 const additions = diff.diffTrimmedLines(old_str, new_str).filter((change) => change.added).map((change) => change.value.trim());
+const deletions = diff.diffTrimmedLines(old_str, new_str).filter((change) => change.removed).map((change) => change.value.trim());
 
 const additions_str = additions.toString().replace(/,/g, '\n');
+const deletions_str = deletions.toString().replace(/,/g, '\n');
 
-core.setOutput("additions", additions_str);
+const additions_arr = additions_str.split(/\n/g);
+const deletions_arr = deletions_str.split(/\n/g);
+
+let changes_str = "";
+for (let i = 0; i < (additions_arr.length > deletions_arr.length ? additions_arr.length : deletions_arr.length); i++) {
+    changes_str += deletions_arr[i] + " -> " + additions_arr[i] + '\n';
+}
+
+core.setOutput("added", additions_str);
+core.setOutput("removed", deletions_str);
+core.setOutput("changed", changes_str);
